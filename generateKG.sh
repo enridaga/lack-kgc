579278403bf1e1d76bc9bf15e8469ec8906990bd
Enrico Daga <enrico.daga@open.ac.uk>
2026-04-28 18:02:05 +0100

    update

--- generateKG.sh
+++ generateKG.sh
@@ -5,7 +5,5 @@ cat output/relations.ttl >> output/KG.ttl
 cat output/provenance-relations.ttl >>  output/KG.ttl
 cat output/provenance-attributes.ttl >>  output/KG.ttl
 cat output/KG-inferred.ttl >> output/KG.ttl
+tar -czf output/KG.tar.gz output/KG.ttl output/KG-inferred.ttl 
 ./stats.sh > KGSTATS.md
-cp -f output/KG.ttl ~/Development/Claude/lack/
-cp -f output/KG-inferred.ttl ~/Development/Claude/lack/
-cp -f KGSTATS.md ~/Development/Claude/lack/
